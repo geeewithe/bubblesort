ac354850ae1ec6972f4db0a5007cc7691aa6a5d2
Asus <Asus@LAPTOP-JLH4NAH6>
2022-09-19 09:51:57 +0530

    bubblesort

--- src/bubblesort.java
+++ src/bubblesort.java
@@ -1,34 +1,25 @@
-public class bubblesort{  
-    static void bubblesort(int[] arr) {  
-        int n = arr.length;  
-        int temp = 0;  
-         for(int i=0; i < n; i++){  
-                 for(int j=1; j < (n-i); j++){  
-                          if(arr[j-1] > arr[j]){           
-                                 temp = arr[j-1];  
-                                 arr[j-1] = arr[j];  
-                                 arr[j] = temp;  
-                         }  
-                          
-                 }  
-         }  
-  
-    }  
-    public static void main(String[] args) {  
-                int arr[] ={6,60,35,2,45,320,5};  
-                 
-                System.out.println("Array Before Bubble Sort");  
-                for(int i=0; i < arr.length; i++){  
-                        System.out.print(arr[i] + " ");  
-                }  
-                System.out.println();  
-                  
-                bubblesort(arr);
-                 
-                System.out.println("Array After Bubble Sort");  
-                for(int i=0; i < arr.length; i++){  
-                        System.out.print(arr[i] + " ");  
-                }  
-   
-        }  
+import java.util.Scanner;
+public class bubbleSsort {
+	public static void bubblesort(int [] arr, int size) {
+		for(int i = 0;i<size;i++) {
+			for(int j = 0;j<size-i-1;j++) {
+				if(arr[j]<arr[i]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int []arr = new int[size];
+		for(int i =0;i<size;i++) {
+			int element = sc.nextInt();
+			arr[i] = element;
+		}
+		bubblesort(arr,size);
+	}
+
 }
